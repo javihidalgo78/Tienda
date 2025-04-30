6977ec96272b6c23a151266b5b9263877752e5e0
javihidalgo78 <jhidlop267@g.educaand.es>
2025-04-30 13:02:12 +0200

    30abril

--- js/funciones.js
+++ js/funciones.js
@@ -1,14 +1,25 @@
 const productos = [
-    {id: 1, nombre: "camiseta", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camiseta.jpg"},
-    {id: 2, nombre: "camiseta", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camiseta.jpg"},
-    {id: 3, nombre: "camiseta", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camiseta.jpg"},
-    {id: 4, nombre: "camiseta", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camiseta.jpg"},
-    {id: 5, nombre: "camiseta", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camiseta.jpg"},  
+    {id: 1, nombre: "R.E.M.", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/camisetarem.jpg"},
+    {id: 2, nombre: "The Beatles", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/beatles.jpg"},
+    {id: 3, nombre: "Pink Floyd", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/pinkfloyd.jpg"},
+    {id: 4, nombre: "Ramones", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/ramones.jpg"},
+    {id: 5, nombre: "U2", precio: 20.00, descripcion: "Manga corta", oferta: false, imagen: "imagenes/u2.jpg"},
+    
+     
 ]
 
-productos.forEach(element =>{
-    document.getElementById("productos").innerHTML = `
-    <img src="${element.imagen}" class="imagenProducto">
-    <p>${element.nombre}</p>
+
+let contenido = "";
+productos.forEach(element => {
+    contenido += `
+        <div class="producto">
+            <img src="${element.imagen}" class="imagenProducto">
+            <div class="infoProducto">
+                <p><strong>${element.nombre}</strong></p>
+                <p>Descripción: ${element.descripcion}</p>
+                <p>Precio: $${element.precio.toFixed(2)}</p>
+            </div>
+        </div>
     `;
 });
+document.getElementById("productos").innerHTML = contenido;
